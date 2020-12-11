f90a503c54a2926963f7a56fdefc39db0792cc89
Osama Ishaq <osama.ishaq10@gmail.com>
2020-12-11 23:55:16 +0500

    Project Initialize

---
+++ app.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const mongoConnect = require("./util/database").mongoConnect;
+
+const errorController = require("./controllers/error");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  //   User.findByPk(1)
+  //     .then((user) => {
+  //       req.user = user;
+  //       console.log("lol");
+  //       next();
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  //   console.log("lol 2");
+});
+
+app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  app.listen(3000);
+});
---
+++ controllers/admin.js
@@ -0,0 +1,105 @@
+const Product = require("../models/product");
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+
+  //! Important: this createProduct is given to us by the sequelize we did not made this func
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+    })
+    .then((result) => {
+      console.log("Product Created Succesfully");
+      return res.redirect("/");
+    })
+    .catch((error) => {
+      console.log("Product Created Failed");
+    });
+};
+
+// exports.getEditProduct = (req, res, next) => {
+//   const editMode = req.query.edit;
+//   if (!editMode) {
+//     return res.redirect("/");
+//   }
+//   const prodId = req.params.productId;
+//   req.user.getProducts({ where: { id: prodId } }).then((products) => {
+//     console.log("This Product ==> ", products[0].title);
+//     if (!products) {
+//       return res.redirect("/");
+//     }
+//     res.render("admin/edit-product", {
+//       pageTitle: "Edit Product",
+//       path: "/admin/edit-product",
+//       editing: editMode,
+//       product: products[0],
+//     });
+//   });
+// };
+
+// exports.postEditProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   const updatedTitle = req.body.title;
+//   const updatedPrice = req.body.price;
+//   const updatedImageUrl = req.body.imageUrl;
+//   const updatedDesc = req.body.description;
+
+//   Product.findByPk(prodId)
+//     .then((product) => {
+//       product.title = updatedTitle;
+//       product.price = updatedPrice;
+//       product.imageUrl = updatedImageUrl;
+//       product.description = updatedDesc;
+
+//       return product.save();
+//     })
+//     .then((result) => {
+//       console.log("Product Updated!!!");
+//       res.redirect("/admin/products");
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
+
+// exports.getProducts = (req, res, next) => {
+//   req.user
+//     .getProducts()
+//     .then((products) => {
+//       res.render("admin/products", {
+//         prods: products,
+//         pageTitle: "Admin Products",
+//         path: "/admin/products",
+//       });
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+// exports.postDeleteProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   Product.findByPk(prodId)
+//     .then((product) => {
+//       return product.destroy();
+//     })
+//     .then((result) => {
+//       console.log("Product Deleted Succesfully");
+//       res.redirect("/admin/products");
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
